4772cadc76dd51a9c9de0c0c1d5f9257c2bbd324
Catriona Beamish <catriona.beamish@gmail.com>
2024-03-14 13:15:41 +0000

    added response print out to see length of variable

--- streamlit.py
+++ streamlit.py
@@ -16,13 +16,9 @@ if video_file is not None:
     with open(vid, mode='wb') as f:
         f.write(video_file.read()) # save video to disk
 
-    st.write('---- saved to disk ----')
-
     vidcap = cv2.VideoCapture(vid)
     success = True
     i = 0
-
-    st.write('---- video capturing ----')
 
     frames = []
     while success:
@@ -30,13 +26,13 @@ if video_file is not None:
     # while i <= 10:
         success, frame = vidcap.read()
         if frame is not None:
-            img = im.fromarray(frame).convert('L')
-            lips = lip_detect(np.array(img))
+            lips = lip_detect(np.array(im.fromarray(frame).convert('L')))
             frames.append(lips.tolist())
             i += 1
             if i % 10 == 0:
                 response = requests.post("https://lip-reader-docker-zn34um6luq-nw.a.run.app/send_frames/", json=json.dumps(frames))
                 if response.ok:
+                    st.write(response.json())
                     frames = []
                 else:
                     st.write(response)
@@ -50,15 +46,9 @@ if video_file is not None:
 
     st.image(lips)
 
-    st.write('---- all frames captured ----')
-
     # AWAITING CORRECT API LINK
     response = requests.get("https://lip-reader-docker-zn34um6luq-nw.a.run.app/predict/")
 
-    st.write('---- post request sent ----')
-
-    st.write(response)
-    st.write(type(response.json()))
     st.write(response.json())
 
     if response.ok:
